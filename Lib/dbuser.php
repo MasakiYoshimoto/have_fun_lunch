8e822f759effd9c26c233acc69bf8ac543104f9f
maronuko <hearteaparty@outlook.jp>
2017-12-16 10:54:18 +0900

    index.CSS修正

--- Lib/dbuser.php
+++ Lib/dbuser.php
@@ -2,7 +2,7 @@
 function getLoginUser($con,$id_user,$pass){
   $sql = "SELECT id_user,first_name,last_name
           FROM m_user
-          WHERE id_user ='".$id_user."' and password = '".$pass."'";
+          WHERE id_user =".$id_user." and password = '".$pass."'";
   $responce = mysqli_query($con,$sql);
   $row = mysqli_fetch_assoc($responce);
   return $row;
